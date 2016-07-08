43ddccc5a5b72948978f58c85f927fa6d316cf1b
Julien Dufresne <juliendufresne@users.noreply.github.com>
2016-07-09 01:27:33 +0200

    Adds verbose option (#10)

--- test.sh
+++ test.sh
@@ -2,10 +2,13 @@
 
 handle_result() {
     local args=$1
+    local output_file=$2
+    local verbose=$3
 
     if [ ${args} -ne 0 ]
     then
         printf "\e[1;31m%s\e[0m\n" "fail"
+        ${verbose} && cat ${output_file}
 
         return 1;
     fi
@@ -13,6 +16,21 @@ handle_result() {
 
     return 0;
 }
+
+VERBOSE=false
+while [[ $# -ge 1 ]]
+do
+    case "$1" in
+        -v|--verbose)
+            VERBOSE=true
+        ;;
+        *)
+            # Unknown option
+            exit 1
+        ;;
+    esac
+    shift
+done
 
 PREVIOUS_VAGRANT_BOX_ID=$(vagrant global-status 2>/dev/null | grep " ansible_role_ubuntu_1604 " | awk '{ print $1; }')
 if [ ! -z "${PREVIOUS_VAGRANT_BOX_ID}" ]
@@ -28,22 +46,23 @@ cd "${REPOSITORY_DIRECTORY}"
 cp -r inventory playbooks requirements.yml Vagrantfile "${TESTING_DIRECTORY}"
 
 cd "${TESTING_DIRECTORY}"
+OUTPUT_FILE=$(mktemp)
 printf "* booting machine: "
-vagrant up --no-provision &>/dev/null
-handle_result $?
+vagrant up --no-provision &>"${OUTPUT_FILE}"
+handle_result $? "${OUTPUT_FILE}" ${VERBOSE}
 
 OUTPUT_FILE=$(mktemp)
 printf "* Fresh provisioning test: "
 vagrant provision &>"${OUTPUT_FILE}"
 grep -q 'unreachable=0.*failed=0' "${OUTPUT_FILE}"
-handle_result $?
+handle_result $? "${OUTPUT_FILE}" ${VERBOSE}
 rm "${OUTPUT_FILE}"
 
 OUTPUT_FILE=$(mktemp)
 printf "* Idempotent test: "
 vagrant provision &>"${OUTPUT_FILE}"
 grep -q 'changed=0.*unreachable=0.*failed=0' "${OUTPUT_FILE}"
-handle_result $?
+handle_result $? "${OUTPUT_FILE}" ${VERBOSE}
 rm "${OUTPUT_FILE}"
 
 vagrant destroy -f >/dev/null
