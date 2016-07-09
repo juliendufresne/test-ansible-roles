1728b71c3f870e094c85d9f54750c9a82842ec9b
Julien Dufresne <juliendufresne@users.noreply.github.com>
2016-07-09 03:25:12 +0200

    Allows to test a role on multiple vagrant box (#16)

--- test.sh
+++ test.sh
@@ -3,12 +3,12 @@
 handle_result() {
     local args=$1
     local output_file=$2
-    local verbose=$3
+    local is_verbose=$3
 
     if [ ${args} -ne 0 ]
     then
         printf "\e[1;31m%s\e[0m\n" "fail"
-        ${verbose} && cat ${output_file}
+        ${is_verbose} && cat ${output_file}
 
         return 1;
     fi
@@ -21,6 +21,57 @@ error() {
     local message="$1"
 
     printf "\e[1;91m%s\e[0m\n\n" "${message}"
+}
+
+run() {
+    local repository_directory="$1"
+    local is_verbose=$2
+    local testing_directory="$(mktemp -d)"
+    local previous_vagrant_box_id=$(vagrant global-status 2>/dev/null | grep " ansible_role_ubuntu_1604 " | awk '{ print $1; }')
+
+    if [ ! -z "${previous_vagrant_box_id}" ]
+    then
+        vagrant destroy -f ${previous_vagrant_box_id}
+    fi
+
+    # Ensure we are in the repository root
+    cd "${repository_directory}"
+    cp -r inventory playbooks requirements.yml Vagrantfile.template "${testing_directory}"
+
+    cd "${testing_directory}"
+
+    # Replace variables in Vagrantfile.template
+    while read -r line ; do
+        while [[ "$line" =~ (\$\{[a-zA-Z_][a-zA-Z_0-9]*\}) ]] ; do
+            LHS=${BASH_REMATCH[1]}
+            RHS="$(eval echo "\"${LHS}\"")"
+            line=${line//${LHS}/${RHS}}
+        done
+        echo "$line" >> Vagrantfile
+    done < Vagrantfile.template
+
+    local output_file=$(mktemp)
+    printf "* booting machine: "
+    vagrant up --no-provision &>"${output_file}"
+    handle_result $? "${output_file}" ${is_verbose}
+
+    local output_file=$(mktemp)
+    printf "* Fresh provisioning test: "
+    vagrant provision &>"${output_file}"
+    grep -q 'unreachable=0.*failed=0' "${output_file}"
+    handle_result $? "${output_file}" ${is_verbose}
+    rm "${output_file}"
+
+    local output_file=$(mktemp)
+    printf "* Idempotent test: "
+    vagrant provision &>"${output_file}"
+    grep -q 'changed=0.*unreachable=0.*failed=0' "${output_file}"
+    handle_result $? "${output_file}" ${is_verbose}
+    rm "${output_file}"
+
+    vagrant destroy -f >/dev/null
+    cd "${repository_directory}"
+    rm -rf "${testing_directory}"
 }
 
 usage() {
@@ -40,8 +91,8 @@ usage() {
 "
 }
 
-VERBOSE=false
-VAGRANT_BOX=
+IS_VERBOSE=false
+VAGRANT_BOXES=
 while [[ $# -ge 1 ]]
 do
     case "$1" in
@@ -50,7 +101,7 @@ do
             exit 0
         ;;
         -v|--verbose)
-            VERBOSE=true
+            IS_VERBOSE=true
         ;;
         --vagrant-box)
             if [ -z "$2" ]
@@ -59,7 +110,7 @@ do
                 usage
                 exit 1
             fi
-            VAGRANT_BOX="$2"
+            VAGRANT_BOXES="${VAGRANT_BOXES} $2"
             shift
         ;;
         *)
@@ -71,55 +122,14 @@ do
     shift
 done
 
-if [ -z "${VAGRANT_BOX}" ]
+if [ -z "${VAGRANT_BOXES}" ]
 then
-    VAGRANT_BOX="geerlingguy/ubuntu1604"
-fi
-
-PREVIOUS_VAGRANT_BOX_ID=$(vagrant global-status 2>/dev/null | grep " ansible_role_ubuntu_1604 " | awk '{ print $1; }')
-if [ ! -z "${PREVIOUS_VAGRANT_BOX_ID}" ]
-then
-    vagrant destroy -f ${PREVIOUS_VAGRANT_BOX_ID}
+    VAGRANT_BOXES="geerlingguy/ubuntu1604"
 fi
 
 REPOSITORY_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-TESTING_DIRECTORY="$(mktemp -d)"
 
-# Ensure we are in the repository root
-cd "${REPOSITORY_DIRECTORY}"
-cp -r inventory playbooks requirements.yml Vagrantfile.template "${TESTING_DIRECTORY}"
-
-cd "${TESTING_DIRECTORY}"
-
-# Replace variables in Vagrantfile.template
-while read -r line ; do
-    while [[ "$line" =~ (\$\{[a-zA-Z_][a-zA-Z_0-9]*\}) ]] ; do
-        LHS=${BASH_REMATCH[1]}
-        RHS="$(eval echo "\"${LHS}\"")"
-        line=${line//${LHS}/${RHS}}
-    done
-    echo "$line" >> Vagrantfile
-done < Vagrantfile.template
-
-OUTPUT_FILE=$(mktemp)
-printf "* booting machine: "
-vagrant up --no-provision &>"${OUTPUT_FILE}"
-handle_result $? "${OUTPUT_FILE}" ${VERBOSE}
-
-OUTPUT_FILE=$(mktemp)
-printf "* Fresh provisioning test: "
-vagrant provision &>"${OUTPUT_FILE}"
-grep -q 'unreachable=0.*failed=0' "${OUTPUT_FILE}"
-handle_result $? "${OUTPUT_FILE}" ${VERBOSE}
-rm "${OUTPUT_FILE}"
-
-OUTPUT_FILE=$(mktemp)
-printf "* Idempotent test: "
-vagrant provision &>"${OUTPUT_FILE}"
-grep -q 'changed=0.*unreachable=0.*failed=0' "${OUTPUT_FILE}"
-handle_result $? "${OUTPUT_FILE}" ${VERBOSE}
-rm "${OUTPUT_FILE}"
-
-vagrant destroy -f >/dev/null
-cd "${REPOSITORY_DIRECTORY}"
-rm -rf "${TESTING_DIRECTORY}"
+for VAGRANT_BOX in ${VAGRANT_BOXES}
+do
+    run "${REPOSITORY_DIRECTORY}" ${IS_VERBOSE}
+done
